9a293e3ce335b493687f2657d56e9a3e42f041a3
Emanuele Aina <em@nerd.ocracy.org>
2007-10-07 23:10:24 +0200

    Simplified the testbench for the 1 bit two-input multiplexer

--- mux21/tb_mux21_1bit.vhdl
+++ mux21/tb_mux21_1bit.vhdl
@@ -1,68 +1,35 @@
-library std;
 library ieee;
-
-use std.textio.all;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_textio.all; -- synopsys only
 
 entity tb_mux21_1bit is
 end tb_mux21_1bit;
 
 architecture test of tb_mux21_1bit is
-    signal A, B, O: std_logic;
-    signal SEL: std_logic;
+    signal A, B, SEL, O: std_logic;
 
-    component mux21_1bit
-        port (A, B: in  std_logic;
-              SEL:  in  std_logic;
-              O:    out std_logic);
+    component mux21_1bit port (
+        A, B: in  std_logic;
+        SEL:  in  std_logic;
+        O:    out std_logic);
     end component;
-
 begin
     U: mux21_1bit port map (A, B, SEL, O);
 
-    test: process
-        variable testA, testB, testO: std_logic;
-        variable testSEL: std_logic;
-        file test_file: text is in "mux21/tb_mux21_1bit.test";
-
-        variable l: line;
-        variable t: time;
-        variable i: integer;
-        variable good: boolean;
-        variable space: character;
+    process
+        constant inputA:    std_logic_vector (0 to 7) := "00001111";
+        constant inputB:    std_logic_vector (0 to 7) := "00110011";
+        constant inputSEL:  std_logic_vector (0 to 7) := "01010101";
+        constant reference: std_logic_vector (0 to 7) := "00011011";
     begin
-        while not endfile(test_file) loop
-            readline(test_file, l);
+        for i in reference'Range loop
+            A   <= inputA(i);
+            B   <= inputB(i);
+            SEL <= inputSEL(i);
 
-            -- read the time from the beginning of the line
-            -- skip the line if it doesn't start with an integer
-            read(l, i, good => good);
-            next when not good;
+            wait for 1 ns;
 
-            read(l, space);
-
-            read(l, testA);
-            read(l, testB);
-            read(l, space);
-
-            read(l, testSEL);
-            read(l, space);
-
-            read(l, testO);
-
-            A <= testA;
-            B <= testB;
-            SEL <= testSEL;
-
-            t := i * 1 ns;  -- convert an integer to time
-            if (now < t) then
-                wait for t - now;
-            end if;
-
-            assert O = testO report "Mismatch on output O";
+            assert O = reference(i);
         end loop;
-
         wait;
     end process;
 end test;
