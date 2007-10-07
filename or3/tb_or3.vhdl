52357fd0db1bd7c874e7587d6ee31f9249683b7e
Emanuele Aina <em@nerd.ocracy.org>
2007-10-07 22:59:29 +0200

    Simplified the three-input OR gate testbench

--- or3/tb_or3.vhdl
+++ or3/tb_or3.vhdl
@@ -1,63 +1,34 @@
-library std;
 library ieee;
-
-use std.textio.all;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_textio.all; -- synopsys only
 
 entity tb_or3 is
 end tb_or3;
 
 architecture test of tb_or3 is
-    signal A: std_logic := '0';
-    signal B: std_logic := '0';
-    signal C: std_logic := '0';
-    signal O: std_logic;
+    signal A, B, C, O: std_logic;
 
     component or3 port (
-        A, B, C: in std_logic;
-        O: out std_logic);
+        A, B, C: in  std_logic;
+        O:       out std_logic);
     end component;
 begin
     U: or3 port map (A, B, C, O);
 
-    test: process
-        variable testA, testB, testC, testO: std_logic;
-        file test_file: text is in "or3/tb_or3.test";
-
-        variable l: line;
-        variable t: time;
-        variable i: integer;
-        variable good: boolean;
-        variable space: character;
+    process
+        constant inputA:    std_logic_vector (0 to 7) := "00001111";
+        constant inputB:    std_logic_vector (0 to 7) := "00110011";
+        constant inputC:    std_logic_vector (0 to 7) := "01010101";
+        constant reference: std_logic_vector (0 to 7) := "01111111";
     begin
-        while not endfile(test_file) loop
-            readline(test_file, l);
+        for i in reference'Range loop
+            A <= inputA(i);
+            B <= inputB(i);
+            C <= inputC(i);
 
-            -- read the time from the beginning of the line
-            -- skip the line if it doesn't start with an integer
-            read(l, i, good => good);
-            next when not good;
+            wait for 1 ns;
 
-            read(l, space); -- skip a space
-
-            read(l, testA);  -- read A value
-            read(l, testB);  -- read B value
-            read(l, testC);  -- read C value
-            read(l, testO);  -- read O value
-
-            A <= testA;
-            B <= testB;
-            C <= testC;
-
-            t := i * 1 ns;  -- convert an integer to time
-            if (now < t) then
-                wait for t - now;
-            end if;
-
-            assert O = testO report "Mismatch on output O";
+            assert O = reference(i);
         end loop;
-
         wait;
     end process;
 end test;
