c2def7d2b16ce3599040a996a5f984220be934dc
Emanuele Aina <em@nerd.ocracy.org>
2007-10-20 15:41:33 +0200

    As the tb now uses 'postponed' avoid using 'inout' for the output port

--- counter/counter.vhdl
+++ counter/counter.vhdl
@@ -6,7 +6,7 @@ entity counter is
     generic (N: integer := 8);
     port (CLK, RST: in    std_logic;
           EN:       in    std_logic;
-          S:        inout std_logic_vector (N-1 downto 0);
+          S:        out   std_logic_vector (N-1 downto 0);
           OFLW:     out   std_logic
     );
 end counter;
@@ -14,17 +14,19 @@ end counter;
 architecture behavioral of counter is
 begin
     process (RST, CLK)
+        variable count: unsigned(S'Range);
     begin
         if RST = '0' then
-            S <= (others => '0');
+            count := (others => '0');
             OFLW <= '0';
         elsif rising_edge(CLK) and EN = '0' then
             OFLW <= '0';
-            if S = (S'Range => '1') then
+            if count = (count'Range => '1') then
                 OFLW <= '1';
             end if;
-            S <= std_logic_vector(unsigned(S)+1);
+            count := count + 1;
         end if;
+        S <= std_logic_vector(count);
     end process;
 end behavioral;
 
@@ -43,15 +45,17 @@ architecture structural of counter is
     end component;
 
     signal carry: std_logic_vector (N downto 0);
-    signal sum: std_logic_vector (N-1 downto 0);
+    signal sum, count: std_logic_vector (N-1 downto 0);
 begin
     carry(0) <= '1';
     OFLW <= carry(N);
 
-    count: for i in N-1 downto 0 generate
-        ha_i: ha port map (S(i), carry(i), sum(i), carry(i+1));
-        fd_en_i: fd_en port map (CLK, RST, EN, sum(i), S(i));
+    count_array: for i in N-1 downto 0 generate
+        ha_i: ha port map (count(i), carry(i), sum(i), carry(i+1));
+        fd_en_i: fd_en port map (CLK, RST, EN, sum(i), count(i));
     end generate;
+
+    S <= count;
 end structural;
 
 
