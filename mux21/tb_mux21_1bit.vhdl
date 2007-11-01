b83066d6c8593dac076066add3c3a351e1e47b7c
Emanuele Aina <em@nerd.ocracy.org>
2007-11-01 22:43:24 +0100

    Use a test vector showing some glitches in the mux21_1bit testbench

--- mux21/tb_mux21_1bit.vhdl
+++ mux21/tb_mux21_1bit.vhdl
@@ -15,21 +15,28 @@ architecture test of tb_mux21_1bit is
 begin
     U: mux21_1bit port map (A, B, SEL, O);
 
-    process
-        constant inputA:    std_logic_vector (0 to 7) := "00001111";
-        constant inputB:    std_logic_vector (0 to 7) := "00110011";
-        constant inputSEL:  std_logic_vector (0 to 7) := "01010101";
-        constant reference: std_logic_vector (0 to 7) := "00011011";
+    A <= '0' after 1 ns, '1' after 2 ns, '0' after 10 ns;
+    B <= '0', '1' after 12 ns, '0' after 12.05 ns,
+              '1' after 13 ns, '0' after 13.11 ns,
+              '1' after 15 ns, '0' after 15.3 ns ;
+    SEL <= '0' after 1 ns, '1' after 3 ns, '0' after 9 ns, '1' after 11 ns;
+
+    check: postponed process
     begin
-        for i in reference'Range loop
-            A   <= inputA(i);
-            B   <= inputB(i);
-            SEL <= inputSEL(i);
-
-            wait for 1 ns;
-
-            assert O = reference(i);
-        end loop;
+        wait for 1.2 ns;
+        assert O = '0';
+        wait for 1 ns; -- 2.2 ns
+        assert O = '1';
+        wait for 1 ns; -- 3.2 ns
+        assert O = '0';
+        wait for 6 ns; -- 9.2 ns
+        assert O = '1';
+        wait for 1 ns; -- 10.2 ns
+        assert O = '0';
+        wait for 5 ns; -- 15.2 ns
+        assert O = '1';
+        wait for 0.3 ns; -- 15.5 ns
+        assert O = '0';
         wait;
     end process;
 end test;
