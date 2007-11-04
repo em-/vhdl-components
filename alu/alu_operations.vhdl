6b848352d94ac5a9cb95561a7eb6907c3b3520db
Emanuele Aina <em@nerd.ocracy.org>
2007-11-04 21:36:06 +0100

    Removed some unused ALU operations

--- alu/alu_operations.vhdl
+++ alu/alu_operations.vhdl
@@ -1,5 +1,5 @@
 package alu_operations is
-    type ALU_OPERATION is (ADD, SUB, ADDS, SUBS, MULT, 
+    type ALU_OPERATION is (ADD, SUB, MULT, 
                         BITAND, BITOR, BITXOR,
                         FUNCLSL, FUNCLSR, FUNCRL, FUNCRR);
 end alu_operations;
